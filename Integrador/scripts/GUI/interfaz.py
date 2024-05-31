ba46d891d6e2ecdd611faa674d2bd6f53b1073f1
Santiago Castro <136345439+Santiago-Castro8@users.noreply.github.com>
2024-05-30 22:55:18 -0300

    Add import tkinter modul and create method for the frames

--- Integrador/scripts/GUI/interfaz.py
+++ Integrador/scripts/GUI/interfaz.py
@@ -1,6 +1,13 @@
+import tkinter as tk
+from tkinter import *
+
 class Interfaz:
     def _init_ (self,root):
         self.root = root
         self.root.title("GESTOR DE FINANZAS")
         self.root.geometry("850x600")
         #self.ventana.attributes("-alpha",0.99)
+    
+    def frames_root(self): 
+        self.barra_superior = tk.Frame(self.root, background= "#1f2329",height=50)
+        self.barra_superior.pack(side= TOP, fill = "both")
