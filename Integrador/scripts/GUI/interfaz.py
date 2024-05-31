96e0ab4ac168e6898b4649db9aa1d820b9e532cd
Santiago Castro <136345439+Santiago-Castro8@users.noreply.github.com>
2024-05-30 22:48:47 -0300

    Class Interfaz

---
+++ Integrador/scripts/GUI/interfaz.py
@@ -0,0 +1,6 @@
+class Interfaz:
+    def _init_ (self,root):
+        self.root = root
+        self.root.title("GESTOR DE FINANZAS")
+        self.root.geometry("850x600")
+        #self.ventana.attributes("-alpha",0.99)
