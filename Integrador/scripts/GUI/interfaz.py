1ac80677ab0231bd9510cae442d420ea1ce5a146
Santiago Castro <136345439+Santiago-Castro8@users.noreply.github.com>
2024-05-30 22:59:45 -0300

    Add main page sidebar with the options

--- Integrador/scripts/GUI/interfaz.py
+++ Integrador/scripts/GUI/interfaz.py
@@ -11,6 +11,9 @@ class Interfaz:
     def frames_root(self): 
         self.barra_superior = tk.Frame(self.root, background= "#1f2329",height=50)
         self.barra_superior.pack(side= TOP, fill = "both")
-        
+
         self.cuerpo = tk.Frame(self.root, background="#f1faff",width =150)
         self.cuerpo.pack(side= RIGHT, fill = "both", expand=True)
+
+        self.menu_lateral = tk.Frame(self.root, background="#2a3138",width= 1500)
+        self.menu_lateral.pack(side=LEFT, fill ="both", expand=False)
