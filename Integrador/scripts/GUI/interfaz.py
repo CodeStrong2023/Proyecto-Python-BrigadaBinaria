f511fad48fc34b2933e9c4159c845d1ecc72b568
Angelina Pizzolatto <132919103+AngelinaPizzolatto@users.noreply.github.com>
2024-06-12 21:50:28 -0300

    Add a method for the title label and a method for the side menu buttons

--- Integrador/scripts/GUI/interfaz.py
+++ Integrador/scripts/GUI/interfaz.py
@@ -17,3 +17,15 @@ class Interfaz:
 
         self.menu_lateral = tk.Frame(self.root, background="#2a3138",width= 1500)
         self.menu_lateral.pack(side=LEFT, fill ="both", expand=False)
+        
+    def etiquetas(self): #etiqueta de título
+        label_titulo = tk.Label(self.barra_superior, text="GESTOR DE FINANZAS PERSONALES")
+        label_titulo.config(foreground="#fff",font=("Helvetica",18),background="#1f2329",pady=20,width=40)
+        label_titulo.pack()
+
+    def botones(self): #botones del menú lateral
+        self.button_subpage_login = tk.Button(self.menu_lateral, text="Mi perfil",command=self.open_subpage_login)
+        self.button_subpage_inc_exp = tk.Button(self.menu_lateral,text="Ingresos y gastos",command=self.open_subpage_inc_exp)
+        self.button_subpage_budget = tk.Button(self.menu_lateral, text="Presupuestos",command=self.open_subpage_budget)
+        self.button_subpage_investments = tk.Button(self.menu_lateral,text="Inversiones",command=self.open_subpage_investments)
+        self.button_export_logs = tk.Button(self.menu_lateral,text="Registros",command=self.open_export_logs)
