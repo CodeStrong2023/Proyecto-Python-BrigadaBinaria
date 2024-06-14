41fd7d7d848eb7060d5be870351f31311eb29755
Santiago Castro <136345439+Santiago-Castro8@users.noreply.github.com>
2024-06-14 20:50:59 -0300

    Add fuction obtener_datos_ingreso

--- Integrador/scripts/ingresos_y_gastos/ingresos.py
+++ Integrador/scripts/ingresos_y_gastos/ingresos.py
@@ -1,3 +1,5 @@
+from Ingresos import Ingresos
+
 class Ingresos: 
     def _init_(self):
         self.conexion = ConectionDB()
@@ -9,3 +11,10 @@ class Ingresos:
         cursor.execute(consulta, valores)
         self.conexion.conexion.commit()
         cursor.close()
+   
+    def obtener_datos_ingreso():
+        date = input("Ingrese la fecha del ingreso (YYYY-MM-DD): ")
+        amount = float(input("Ingrese el monto del ingreso: "))
+        category = input("Ingrese la categoría del ingreso: ")
+        description = input("Ingrese la descripción del ingreso: ")
+        return date, amount, category, description
