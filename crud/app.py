d5e873995ac96d418ec71a9004c7d7410fd0f735
Angelina Pizzolatto <132919103+AngelinaPizzolatto@users.noreply.github.com>
2024-06-27 18:28:03 -0300

    Agrego rutas de páginas secundarias en app.py

--- crud/app.py
+++ crud/app.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/budget')
+def budget():
+    return render_template('budget.html')
+
+@app.route('/inc_exp')
+def incomes_expenses():
+    return render_template('inc_exp.html')
+
+@app.route('/registrarse')
+def registrarse():
+    return render_template('registrarse.html')
